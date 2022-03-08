65b91918a55b6f8a5450d879d4050dc2273ca62c
ElectricBeat <96882916+ElectricBeat@users.noreply.github.com>
2022-03-08 21:07:16 +0530

    LYCASOFT
    LYCASOFT TRINNING CODE FOR ARDUINO UNO LCD 0-9 CODE

---
+++ LCD_COUNT_0-9.ino
@@ -0,0 +1,25 @@
+#include <LiquidCrystal.h> 
+LiquidCrystal lcd(7, 6, 5, 4, 3, 2);
+
+int count=0;
+
+void setup() 
+{
+  lcd.begin(16, 2);
+}
+void loop() 
+{
+  lcd.setCursor(0,0);
+  lcd.print("SRI ELECTRONIC & EMBEDDED SOLUTIONS");
+  delay(2000);
+   
+  lcd.setCursor(0,1);
+  lcd.print("Count:");
+  
+  for(count=0;count<10;count++)
+  {
+    lcd.setCursor(6,1); 
+    lcd.print(count);
+    delay(1000);
+  }  
+}
